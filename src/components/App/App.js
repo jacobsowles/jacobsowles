b3b556b239bdea2947f783827aa075906866e1d5
Jacob Sowles <jrsowles@gmail.com>
2018-11-07 17:55:52 -0800

    move SectionHeader and SectionBody into Section component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,6 @@ import { far, faAddressCard, faBuilding } from '@fortawesome/free-regular-svg-ic
 import { faMapMarkerAlt, faCogs } from '@fortawesome/free-solid-svg-icons';
 import Profile from '../Profile/Profile';
 import Section from '../Section/Section';
-import SectionBody from '../SectionBody/SectionBody';
-import SectionHeader from '../SectionHeader/SectionHeader';
 import SocialLinkList from '../SocialLinkList/SocialLinkList';
 import SocialLinkListItem from '../SocialLinkListItem/SocialLinkListItem';
 
@@ -34,16 +32,12 @@ class App extends Component {
 					<div className="col-3"></div>
 
 					<div className="col-6">
-						<Section>
-							<SectionHeader title="Things I'm currently excited about" />
-
-							<SectionBody>
-								<ul>
-									<li>Effortlessly processing receipts with Zapier and Hazel</li>
-									<li>Automatically cataloging business card contacts with optical character recognition and the Airtable API</li>
-									<li>Jumpstarting my daily guitar practice with Evernote, Harvest, and Keyboard Maestro</li>
-								</ul>
-							</SectionBody>
+						<Section title="Things I'm currently excited about">
+							<ul>
+								<li>Effortlessly processing receipts with Zapier and Hazel</li>
+								<li>Automatically cataloging business card contacts with optical character recognition and the Airtable API</li>
+								<li>Jumpstarting my daily guitar practice with Evernote, Harvest, and Keyboard Maestro</li>
+							</ul>
 						</Section>
 					</div>
 
@@ -54,28 +48,24 @@ class App extends Component {
 					<div className="col-3"></div>
 
 					<div className="col-6">
-						<Section>
-							<SectionHeader title="Get in touch" />
-
-							<SectionBody>
-								<SocialLinkList>
-									<SocialLinkListItem
-										icon={faLinkedin}
-										url="https://www.linkedin.com/in/jacobsowles/"
-										title="LinkedIn"
-									/>
-									<SocialLinkListItem
-										icon={faTwitter}
-										url="https://twitter.com/jacobsowles"
-										title="Twitter"
-									/>
-									<SocialLinkListItem
-										icon={faGithub}
-										url="https://github.com/jacobsowles"
-										title="Github"
-									/>
-								</SocialLinkList>
-							</SectionBody>
+						<Section title="Get in touch">
+							<SocialLinkList>
+								<SocialLinkListItem
+									icon={faLinkedin}
+									url="https://www.linkedin.com/in/jacobsowles/"
+									title="LinkedIn"
+								/>
+								<SocialLinkListItem
+									icon={faTwitter}
+									url="https://twitter.com/jacobsowles"
+									title="Twitter"
+								/>
+								<SocialLinkListItem
+									icon={faGithub}
+									url="https://github.com/jacobsowles"
+									title="Github"
+								/>
+							</SocialLinkList>
 						</Section>
 					</div>
 
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,12 +1,21 @@
 import './Section.scss';
 
 import React, { Component } from 'react';
+import SectionBody from './SectionBody/SectionBody';
+import SectionHeader from './SectionHeader/SectionHeader';
 
 class Section extends Component {
 	render() {
 		return (
 			<div className="section">
-				{this.props.children}
+				<SectionHeader
+					title={this.props.title}
+					subtitle={this.props.subtitle}
+				/>
+
+				<SectionBody>
+					{this.props.children}
+				</SectionBody>
 			</div>
 		);
 	}
