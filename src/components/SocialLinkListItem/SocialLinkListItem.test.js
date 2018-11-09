96d7f7135206085f9fed01330cf7667803130993
Jacob Sowles <jrsowles@gmail.com>
2018-11-08 19:00:02 -0800

    fix intentionally broken test

--- src/components/SocialLinkListItem/SocialLinkListItem.test.js
+++ src/components/SocialLinkListItem/SocialLinkListItem.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import SocialLinkListItem from './SocialLinkListItem';
 
 it('renders without crashing', () => {
-	const div = React.createElement('div');
+	const div = document.createElement('div');
 	ReactDOM.render(<SocialLinkListItem />, div);
 	ReactDOM.unmountComponentAtNode(div);
 })
