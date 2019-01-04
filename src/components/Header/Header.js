9ff7f9a581d92db107679a41acc5c8dd7ba74d9f
Jacob Sowles <jrsowles@gmail.com>
2019-01-04 11:59:21 -0800

    refactor: remove unneeded graphql query

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,45 +11,36 @@ function Header({ isRootPath, title }) {
   const titleHtml = isRootPath ? <h1>{title}</h1> : <h3>{title}</h3>;
 
   return (
-    <StaticQuery
-      query={headerQuery}
-      render={data => {
-        const { author } = data.site.siteMetadata;
+    <Navbar className="header">
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link className="home-link" to={`/`}>
+            <Logo />
 
-        return (
-          <Navbar className="header">
-            <Navbar.Header>
-              <Navbar.Brand>
-                <Link className="home-link" to={`/`}>
-                  <Logo />
-
-                  <div className="header-text-wrapper">
-                    {titleHtml}
-                    <p className="tagline">
-                      Full-stack developer. Aspiring everything-elser.
-                    </p>
-                  </div>
-                </Link>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav pullRight>
-                <li>
-                  <Link to="/">Blog</Link>
-                </li>
-                <li>
-                  <Link to="/skills">Skills</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        );
-      }}
-    />
+            <div className="header-text-wrapper">
+              {titleHtml}
+              <p className="tagline">
+                Full-stack developer. Aspiring everything-elser.
+              </p>
+            </div>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <li>
+            <Link to="/">Blog</Link>
+          </li>
+          <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
@@ -57,22 +48,5 @@ Header.propTypes = {
   isRootPath: PropTypes.bool.isRequired,
   title: PropTypes.string.isRequired,
 };
-
-const headerQuery = graphql`
-  query HeaderQuery {
-    avatar: file(absolutePath: { regex: "/profile-photo.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    site {
-      siteMetadata {
-        author
-      }
-    }
-  }
-`;
 
 export default Header;
