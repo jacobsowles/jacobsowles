83f0b63a8ebcaa6ee5b596cc750b8c8354d9695e
Jacob Sowles <jrsowles@gmail.com>
2019-01-07 10:18:55 -0800

    feat(WordCloud): update skills word cloud

--- src/components/WordCloud/WordCloud.js
+++ src/components/WordCloud/WordCloud.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './WordCloud.scss';
 
 const WordCloud = () => (
-  <img className="wordcloud" src="skills-wordcloud.png" alt="word cloud" />
+  <img className="wordcloud" src="/skills-wordcloud.png" alt="word cloud" />
 );
 
 export default WordCloud;
