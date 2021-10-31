878fbdb07b46a1f301197cfdf712daf873b12644
Jacob Sowles <jrsowles@gmail.com>
2021-10-31 12:54:31 -0700

    Render data

--- jacobsowles/apps/jacobsowles/src/components/SeatTime/SeatTime.tsx
+++ jacobsowles/apps/jacobsowles/src/components/SeatTime/SeatTime.tsx
@@ -8,10 +8,9 @@ type TimeRecord = {
 }
 
 const SeatTime = () => {
-
   const { data, error, isLoading } = useFetch<TimeRecord[]>("https://us-central1-seat-time-330421.cloudfunctions.net/app/time-records");
 
-  return isLoading ? <p>Loading...</p> : error ? <p>{error.message}</p> : <Fragment>{data}</Fragment>;
+  return isLoading ? <p>Loading...</p> : error ? <p>{error.message}</p> : <ul>{data?.map((timeRecord, key) => <li key={key}>{timeRecord.duration} milliseconds at {timeRecord.timestamp}</li>)}</ul>;
 };
 
 export { SeatTime };
